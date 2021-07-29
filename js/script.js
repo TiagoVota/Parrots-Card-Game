7add088a4b832163e214da1039916d88a25f4598
Tiagovota <tiagovotacucco@gmail.com>
2021-07-29 14:09:46 -0300

    Alterações no layout e insersão de lógica para verificação da resposta

--- js/script.js
+++ js/script.js
@@ -2,7 +2,32 @@
 * Essa função é showdi
 * @param {number} n1 é o primeiro número
 * @param {number} n2 é o segundo número
+* @return {number} a soma dos carinhas
 */
 function teste(n1, n2) {
     return n1 + n2
 }
+
+
+function isValidAnswer(answer) {
+    answer = Number(answer)  // Vê o estado numérico da resposta
+    
+    if (Number.isFinite(answer)) {
+
+        // Se for um número finito, verifica se é um número múltiplo de 2
+        answer = Number(answer) / 2
+        const isEven = parseInt(answer, 10) == answer
+        if (isEven) {
+
+            // Verifica se a metade do número está entre 1 e 7
+            if (1 <= answer && answer <= 7) {
+
+                return true
+            }
+        }  
+    }
+
+    // Caso não seja um número par entre 2 e 14
+    return false
+}
+
